456cf5bf992039b96a2e05b16a328526cae87943
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-27 15:09:04 -0500

    first commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * find-file-up <https://github.com/jonschlinkert/find-file-up>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
