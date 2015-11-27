2c3a356794dcfbe1756995f87c13f18deca1253e
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-27 16:06:19 -0500

    first pass

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * find-file-up <https://github.com/jonschlinkert/find-file-up>
+ * find-pkg <https://github.com/jonschlinkert/find-pkg>
  *
  * Copyright (c) 2015, Jon Schlinkert.
  * Licensed under the MIT License.
@@ -7,6 +7,56 @@
 
 'use strict';
 
-module.exports = function () {
-  // do stuff
+/**
+ * Module dependencies
+ */
+
+var fs = require('fs');
+var path = require('path');
+var resolve = require('resolve-dir');
+var cwd = process.cwd();
+
+/**
+ * Find a file, starting with the given directory
+ */
+
+module.exports = function findFile(filename, dir, cb) {
+  if (typeof dir === 'function') {
+    cb = dir;
+    dir = '.';
+  }
+
+  dir = resolve(dir);
+
+  function find(dir, next) {
+    var fp = path.resolve(dir, filename);
+    fs.exists(fp, function(exists) {
+      if (exists) {
+        cb(null, fp);
+        return;
+      }
+      if (dir === cwd || dir === '.' || dir === path.sep) {
+        next();
+      } else {
+        find(path.dirname(dir), next);
+      }
+    });
+  }
+  find(resolve(dir), cb);
+};
+
+module.exports.sync = function findFileSync(filename, dir) {
+  dir = dir ? resolve(dir) : '.';
+
+  function find(dir) {
+    var fp = path.resolve(dir, filename);
+    if (fs.existsSync(fp)) {
+      return fp;
+    }
+    if (dir === cwd || dir === '.' || dir === path.sep) {
+      return;
+    }
+    return find(path.dirname(dir));
+  }
+  return find(resolve(dir));
 };
---
+++ test.js
@@ -0,0 +1,87 @@
+/*!
+ * find-pkg <https://github.com/jonschlinkert/find-pkg>
+ *
+ * Copyright (c) 2015 .
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+require('mocha');
+var fs = require('fs');
+var path = require('path');
+var resolve = require('resolve-dir');
+var assert = require('assert');
+var findFile = require('./');
+var del = require('delete');
+
+describe('find-file', function() {
+  it('should resolve a file in the cwd:', function(cb) {
+    findFile('index.js', '.', function(err, fp) {
+      assert.equal(fp, path.resolve('index.js'));
+      cb();
+    });
+  });
+
+  it('should resolve a file from user home:', function(cb) {
+    fs.writeFile(resolve('~/__foo.js'), 'tmp', function(err) {
+      if (err) return cb(err);
+
+      findFile('__foo.js', '~/_a/b/c', function(err, fp) {
+        assert(fp === resolve('~/__foo.js'));
+        del(fp, {force: true}, cb);
+      });
+    });
+  });
+
+  it('should return undefined when the file does not exist:', function(cb) {
+    findFile('whatever.js', resolve('~/a/b/c'), function(err, fp) {
+      assert(fp === undefined);
+      cb();
+    });
+  });
+
+  it('should resolve a file from one directory up:', function(cb) {
+    findFile('index.js', 'test', function(err, fp) {
+      assert.equal(fp, path.resolve('index.js'));
+      cb();
+    });
+  });
+
+  it('should resolve index.js from multiple directories up:', function(cb) {
+    findFile('index.js', 'test/a/b/c/c/', function(err, fp) {
+      assert.equal(fp, path.resolve('index.js'));
+      cb();
+    });
+  });
+});
+
+describe('find-file-sync', function() {
+  it('should resolve a file in the cwd:', function() {
+    var fp = findFile.sync('index.js', '.');
+    assert.equal(fp, path.resolve('index.js'));
+  });
+
+  it('should resolve a file from user home:', function() {
+    fs.writeFileSync(resolve('~/__foo.js'), 'tmp');
+
+    var fp = findFile.sync('__foo.js', '~/_a/b/c');
+    assert(fp === resolve('~/__foo.js'));
+    del.sync(fp, {force: true});
+  });
+
+  it('should return undefined when the file does not exist:', function() {
+    var fp = findFile.sync('whatever.js', resolve('~/a/b/c'));
+    assert(fp === undefined);
+  });
+
+  it('should resolve a file from one directory up:', function() {
+    var fp = findFile.sync('index.js', 'test');
+    assert.equal(fp, path.resolve('index.js'));
+  });
+
+  it('should resolve index.js from multiple directories up:', function() {
+    var fp = findFile.sync('index.js', 'test/a/b/c/c/');
+    assert.equal(fp, path.resolve('index.js'));
+  });
+});
